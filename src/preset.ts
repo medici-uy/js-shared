70d971a4fd75437b1bf214f53ddd68c3dfa1df62
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2024-03-28 22:29:36 -0300

    move to light-dark

--- src/preset.ts
+++ src/preset.ts
@@ -18,194 +18,110 @@ export const preset = definePreset({
             value: 'light-dark({colors.base.primaryText}, oklch(95% 0 0))',
           },
           secondaryText: {
-            value: {
-              base: 'oklch(42% 0 0)',
-              _dark: 'oklch(85% 0 0)',
-            },
+            value: 'light-dark(oklch(42% 0 0), oklch(85% 0 0))',
           },
           tertiaryText: {
-            value: {
-              base: 'oklch(56% 0 0)',
-              _dark: 'oklch(70% 0 0)',
-            },
+            value: 'light-dark(oklch(56% 0 0), oklch(70% 0 0))',
           },
           bg: {
             value: 'light-dark({colors.white}, oklch(17.5% 0.012 254))',
           },
           primary: {
             '1': {
-              value: {
-                base: 'oklch(89% 0.05 263)',
-                _dark: 'oklch(19% 0.07 253)',
-              },
+              value: 'light-dark(oklch(89% 0.05 263), oklch(19% 0.07 253))',
             },
             '2': {
-              value: {
-                base: 'oklch(69% 0.16 263)',
-                _dark: 'oklch(37% 0.18 263)',
-              },
+              value: 'light-dark(oklch(69% 0.16 263), oklch(37% 0.18 263))',
             },
             '3': {
-              value: {
-                base: '{colors.base.primary.3}',
-                _dark: 'oklch(60% 0.23 263)',
-              },
+              value: 'light-dark({colors.base.primary.3}, oklch(60% 0.23 263))',
             },
             '4': {
-              value: {
-                base: 'oklch(37% 0.18 263)',
-                _dark: 'oklch(69% 0.16 263)',
-              },
+              value: 'light-dark(oklch(37% 0.18 263), oklch(69% 0.16 263))',
             },
             '5': {
-              value: {
-                base: 'oklch(19% 0.07 253)',
-                _dark: 'oklch(89% 0.05 263)',
-              },
+              value: 'light-dark(oklch(19% 0.07 253), oklch(89% 0.05 263))',
             },
           },
           accent: {
-            value: {
-              base: 'oklch(86.4% 0.2 90)',
-              _dark: 'oklch(76% 0.15 298)',
-            },
+            value: 'light-dark(oklch(86.4% 0.2 90), oklch(76% 0.15 298))',
           },
           red: {
             '1': {
-              value: {
-                base: 'oklch(89% 0.06 28)',
-                _dark: 'oklch(24% 0.06 28)',
-              },
+              value: 'light-dark(oklch(89% 0.06 28), oklch(24% 0.06 28))',
             },
             '2': {
-              value: {
-                base: 'oklch(70% 0.14 28)',
-                _dark: 'oklch(43% 0.15 28)',
-              },
+              value: 'light-dark(oklch(70% 0.14 28), oklch(43% 0.15 28))',
             },
             '3': {
-              value: {
-                base: 'oklch(58% 0.22 28)',
-                _dark: 'oklch(60% 0.18 28)',
-              },
+              value: 'light-dark(oklch(58% 0.22 28), oklch(60% 0.18 28))',
             },
             '4': {
-              value: {
-                base: 'oklch(40% 0.15 28)',
-                _dark: 'oklch(71% 0.14 28)',
-              },
+              value: 'light-dark(oklch(40% 0.15 28), oklch(71% 0.14 28))',
             },
             '5': {
-              value: {
-                base: 'oklch(20% 0.06 28)',
-                _dark: 'oklch(89% 0.06 28)',
-              },
+              value: 'light-dark(oklch(20% 0.06 28), oklch(89% 0.06 28))',
             },
           },
           yellow: {
             '1': {
-              value: {
-                base: 'oklch(96% 0.07 98)',
-                _dark: 'oklch(32% 0.06 98)',
-              },
+              value: 'light-dark(oklch(96% 0.07 98), oklch(32% 0.06 98))',
             },
             '2': {
-              value: {
-                base: 'oklch(91% 0.15 98)',
-                _dark: 'oklch(62% 0.14 98)',
-              },
+              value: 'light-dark(oklch(91% 0.15 98), oklch(62% 0.14 98))',
             },
             '3': {
-              value: { base: 'oklch(85% 0.2 98)', _dark: 'oklch(87% 0.18 98)' },
+              value: 'light-dark(oklch(85% 0.2 98), oklch(87% 0.18 98))',
             },
             '4': {
-              value: {
-                base: 'oklch(59% 0.14 98)',
-                _dark: 'oklch(92% 0.15 98)',
-              },
+              value: 'light-dark(oklch(59% 0.14 98), oklch(92% 0.15 98))',
             },
             '5': {
-              value: {
-                base: 'oklch(28% 0.06 98)',
-                _dark: 'oklch(96% 0.07 98)',
-              },
+              value: 'light-dark(oklch(28% 0.06 98), oklch(96% 0.07 98))',
             },
           },
           green: {
             '1': {
-              value: {
-                base: 'oklch(95% 0.06 140)',
-                _dark: 'oklch(30% 0.07 140)',
-              },
+              value: 'light-dark(oklch(95% 0.06 140), oklch(30% 0.07 140))',
             },
             '2': {
-              value: {
-                base: 'oklch(86% 0.17 140)',
-                _dark: 'oklch(57% 0.16 140)',
-              },
+              value: 'light-dark(oklch(86% 0.17 140), oklch(57% 0.16 140))',
             },
             '3': {
-              value: {
-                base: 'oklch(79% 0.24 140)',
-                _dark: 'oklch(81% 0.2 140)',
-              },
+              value: 'light-dark(oklch(79% 0.24 140), oklch(81% 0.2 140))',
             },
             '4': {
-              value: {
-                base: 'oklch(54% 0.16 140)',
-                _dark: 'oklch(87% 0.17 140)',
-              },
+              value: 'light-dark(oklch(54% 0.16 140), oklch(87% 0.17 140))',
             },
             '5': {
-              value: {
-                base: 'oklch(26% 0.07 140)',
-                _dark: 'oklch(95% 0.06 140)',
-              },
+              value: 'light-dark(oklch(26% 0.07 140), oklch(95% 0.06 140))',
             },
           },
           gray: {
             '1': {
-              value: {
-                base: '{colors.base.gray.1}',
-                _dark: 'oklch(24% 0.006 286)',
-              },
+              value: 'light-dark({colors.base.gray.1}, oklch(24% 0.006 286))',
             },
             '2': {
-              value: {
-                base: 'oklch(75% 0.014 286)',
-                _dark: 'oklch(43% 0.016 286)',
-              },
+              value: 'light-dark(oklch(75% 0.014 286), oklch(43% 0.016 286))',
             },
             '3': {
-              value: {
-                base: 'oklch(58% 0.025 286)',
-                _dark: 'oklch(60% 0.025 286)',
-              },
+              value: 'light-dark(oklch(58% 0.025 286), oklch(60% 0.025 286))',
             },
             '4': {
-              value: {
-                base: 'oklch(41% 0.016 286)',
-                _dark: 'oklch(76% 0.014 286)',
-              },
+              value: 'light-dark(oklch(41% 0.016 286), oklch(76% 0.014 286))',
             },
             '5': {
-              value: {
-                base: 'oklch(21% 0.006 286)',
-                _dark: 'oklch(92% 0.004 286)',
-              },
+              value: 'light-dark(oklch(21% 0.006 286), oklch(92% 0.004 286))',
             },
           },
           divider: {
-            value: { base: '{colors.gray.1}', _dark: '{colors.gray.1}' },
+            value: '{colors.gray.1}',
           },
           icon: {
-            value: {
-              base: '{colors.primaryText}',
-              _dark: '{colors.primaryText}',
-            },
+            value: '{colors.primaryText}',
           },
           input: {
-            value: { base: 'oklch(96% 0 0)', _dark: 'oklch(10.5% 0.021 252)' },
+            value: 'light-dark(oklch(96% 0 0), oklch(10.5% 0.021 252))',
           },
         },
         spacing: {
