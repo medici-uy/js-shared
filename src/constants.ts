67ac61191d8292511224a1e8ad29f8e68ff165a7
Diego Stratta <18295954+dbstratta@users.noreply.github.com>
2024-12-16 18:04:48 -0300

    add constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,11 @@ export const primaryColorHex = {
 
 export const appDisplayName = "medici";
 export const appDescription = "El banco de preguntas de la FMED Udelar.";
+
+export const mediciInstagramUsername = "medici.uy";
+export const mediciXUsername = "medici_uy";
+export const mediciTikTokUsername = "medici.uy";
+
+export const mediciInstagramUrl = `https://instagram.com/${mediciInstagramUsername}`;
+export const mediciXUrl = `https://x.com/${mediciXUsername}`;
+export const mediciTikTokUrl = `https://tiktok.com/@${mediciTikTokUsername}`;
